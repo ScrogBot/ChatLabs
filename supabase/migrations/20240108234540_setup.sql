d48962700d6ce7c9aea0a0a4e7a4d6c85df2827a
ScrogBot <141583099+ScrogBot@users.noreply.github.com>
2024-04-23 14:18:43 -0500

    Update 20240108234540_setup.sql

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,8 +50,8 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'https://ukrdhiywigevlawgvmbj.supabase.co';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrcmRoaXl3aWdldmxhd2d2bWJqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzI1MjAzMiwiZXhwIjoyMDIyODI4MDMyfQ.rCz1Zqjy0cCZXzW8idiLsjAyz5fZxy7mMQKau02BQ_s'; -- full access needed for http request to storage
+  project_url TEXT := 'https://npdvubrxswmabgosqfow.supabase.co';
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wZHZ1YnJ4c3dtYWJnb3NxZm93Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwODk3NTA1NSwiZXhwIjoyMDI0NTUxMDU1fQ.G91JpSUC7DZlZbMcSrA2t7prjWJxzxUBRL3k1m1ERVM'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
